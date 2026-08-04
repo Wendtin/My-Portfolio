c0a738a6f7845e093a4d284ce6c863421363f767
Wendtin <sambilezony@gmail.com>
2026-08-04 19:08:02 -0400

    Add description to security group rule

--- main.tf
+++ main.tf
@@ -1,15 +1,16 @@
-provider "aws" {  
-  region = "us-east-1"  
-}  
-  
-resource "aws_security_group" "sabotaged_sg" {  
-  name        = "tlab7-exposed-sg"  
-  description = "A dangerously exposed security group"  
-  
-  ingress {  
-    from_port   = 22  
-    to_port     = 22  
-    protocol    = "tcp"  
-    cidr_blocks = ["108.14.201.176/32"] # SABOTAGE: SSH exposed to the world  
-  }  
-}  
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "sabotaged_sg" {
+  name        = "tlab7-exposed-sg"
+  description = "A dangerously exposed security group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    description = "SSH access from home IP only"
+    cidr_blocks = ["108.14.201.176/32"]
+  }
+}
