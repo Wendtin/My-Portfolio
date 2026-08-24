bee96869a1c5354686dcc732f97796343b87bf53
Wendtin <sambilezony@gmail.com>
2026-08-24 14:36:18 -0400

    fix: remediate tfsec security findings

--- main.tf
+++ main.tf
@@ -82,9 +82,9 @@ resource "aws_security_group" "web_sg" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
